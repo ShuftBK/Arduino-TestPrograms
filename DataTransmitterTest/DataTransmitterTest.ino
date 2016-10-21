c25f1ae79344471e5f98a59205cc56a02f78b49e
Shuft <shuft@coils.work>
2016-10-22 02:05:24 +0900

    make arduino test programs

---
+++ DataTransmitterTest/DataTransmitterTest.ino
@@ -0,0 +1,16 @@
+/*
+ * DataTransmitterTest.ino
+ * Transmitting data to other Devises
+ */
+
+const int Speed = 9600;
+
+void setup() {
+  Serial.begin(Speed);
+}
+
+void loop() {
+  if(Serial.available()){
+  Serial.println(Serial.read());
+  }
+}
